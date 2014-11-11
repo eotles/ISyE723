542a8242f9c33d28bcf05b871c298be1ebffae6d
eotles <eotles@gmail.com>
2014-11-11 15:34:52 -0600

    got value iteration working!

--- hw5.py
+++ hw5.py
@@ -8,19 +8,22 @@ import markovDecisionProblem as mdp
 
 def main():
     N = float("inf")
-    S = ['s_1','s_2']
-    A = {'s_1': ['a_1,1','a_1,2'],
-         's_2': ['a_2,1']}
-    r_t = {mdp.stateAndAction('s_1', 'a_1,1'): 5,
-           mdp.stateAndAction('s_1', 'a_1,2'): 10,
-           mdp.stateAndAction('s_2', 'a_2,1'): -1}
+    S = ['L','H']
+    A = {'L': ['Send','DoNot'],
+         'H': ['Send','DoNot']}
+    r_t = {mdp.stateAndAction('L', 'Send'): 20-15,
+           mdp.stateAndAction('L', 'DoNot'): 10,
+           mdp.stateAndAction('H', 'Send'): 50-15,
+           mdp.stateAndAction('H', 'DoNot'): 25}
     r_N = None
-    p =   {mdp.stateAndAction('s_1', 'a_1,1'): {'s_1': 0.5, 's_2': 0.5},
-           mdp.stateAndAction('s_1', 'a_1,2'): {'s_1': 0,   's_2': 1},
-           mdp.stateAndAction('s_2', 'a_2,1'): {'s_1': 0,   's_2': 1}}
+    p =   {mdp.stateAndAction('L', 'Send'):  {'L':0.3,'H':0.7},
+           mdp.stateAndAction('L', 'DoNot'): {'L':0.5,'H':0.5},
+           mdp.stateAndAction('H', 'Send'):  {'L':0.2,'H':0.8},
+           mdp.stateAndAction('H', 'DoNot'): {'L':0.6,'H':0.4}}
+    l = 0.9
     
-    model = mdp.model(N, S, A, r_t, r_N, p)
-    model.linearProgramming()
+    model = mdp.model(N, S, A, r_t, r_N, p, l)
+    model.valueIteration(0.01)
 
 
 if __name__ == '__main__':
--- markovDecisionProblem.py
+++ markovDecisionProblem.py
@@ -4,6 +4,8 @@ Created on Nov 11, 2014
 @author: eotles
 '''
 import collections
+import math
+import sys
 
 stateAndAction = collections.namedtuple("stateAndAction", ['state', 'action'])
 
@@ -16,10 +18,11 @@ class improperTimeHorizonException(Exception):
 
 class model(object):
     
-    def __init__(self, N, S, A, r_t, r_N, p):
+    def __init__(self, N, S, A, r_t, r_N, p, l):
         self.N = N
         if(N == float("inf")):
             self.infHoriz = True
+            self.l = l
         else:
             self.infHoriz = False
             self.r_N = r_N
@@ -31,16 +34,56 @@ class model(object):
     def backwardInduction(self):
         if self.infHoriz: raise(improperTimeHorizonException("Try An Infinite Time Horizon Method"))
       
-    def valueIteration(self):
+    def valueIteration(self, epsilon):
         if (not self.infHoriz): raise(improperTimeHorizonException("Try A Finite Time Horizon Method"))
+        convergenceLimit = epsilon*(1-self.l)/(2*self.l)
+        v = dict()
+        d = dict()
         
         #1 - select v_0 in V, specify E>0, set n=0
+        n = 0
+        tempV = dict()
+        for state in self.S:
+            tempV.update({state: 0})
+        v.update({n: tempV})
         
-        #2 - for each s in S, compute v_n+1 = max_wrt_a_in_As(r(s,a) + sum_over_j_in_S(l*p(j|s,a)*v_n(j))
-        
+        doStep2 = True
         #3 - if ||v_n+1 - v_n|| < E(1-l)/2l goto: 4 else n++ goto 2
+        while(doStep2):
+            n+=1
+            #2 - for each s in S, compute v_n+1 = max_wrt_a_in_As(r(s,a) + sum_over_j_in_S(l*p(j|s,a)*v_n(j))
+            tempV = dict()
+            tempD = dict()
+            for state in self.S:
+                maxReward = sys.float_info.min
+                argMaxReward = None                
+                for action in self.A.get(state):
+                    saPair = stateAndAction(state, action)
+                    reward = self.r_t.get(saPair)
+                    for nextState, transProb in self.p.get(saPair).iteritems():
+                        reward += self.l * transProb * v.get(n-1).get(nextState)
+                    if(reward > maxReward):
+                        maxReward = reward
+                        argMaxReward = action
+                tempV.update({state: maxReward})
+                tempD.update({state: argMaxReward})
+            v.update({n: tempV})
+            d.update({n: tempD})
+                
+            #3 - if ||v_n+1 - v_n|| < E(1-l)/2l goto: 4 else n++ goto 2
+            distance = 0
+            for state in self.S:
+                distance += (v.get(n).get(state)-v.get(n-1).get(state))**2
+            l2Norm = math.sqrt(distance)
+            if(l2Norm < convergenceLimit):
+                doStep2 = False
+        
+        #print(d)
+        #print(v)
         
         #4 - for s in S choose d_E(s) belong to argmax_wrt_a_in_As(r(s,a) + sum_over_j_in_S(l*p(j|s,a)*v_n+1(j))
+        for state in self.S:
+            print("State: %s, Decision: %s, Value: %f" %(state, d.get(n).get(state), v.get(n).get(state)))
         
         
             
